65d35539c8ef2e4e44ca1f6b6c201eca95c3a93b
Scott Fodje <sfodje@turnitin.com>
2016-03-21 08:28:19 -0700

    Added command line flags and more test coverage

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 This repo contains my solutions to questions found on https://projecteuler.net
 Started this as a means to practice TDD and my solutions are still a work in progress.
+All solutions are written in the Go programming language.
 
 ##Installation
 ```
 go get github.com/sfodje/project_euler/...
 ```
 
+##Usage
+```
+$ project_euler -problem=5
+Problem 5: Smallest multiple of all numbers (1 to 20) = 232792560
 
+$ project_euler -problem=2 -end=7
+Problem 2: Sum of even fibonacci numbers 4,000,000 and below = 4613732
+Problem 3: Largest prime factor of 600851475143 = 6857
+Problem 4: Largest palindrome from product of 2 3-digit numbers = 906609
+Problem 5: Smallest multiple of all numbers (1 to 20) = 232792560
+Problem 6: Difference between the square of sum and sum of squares (1 to 100) = 25164150
+Problem 7: 10001th prime number = 104743
+```
--- main.go
+++ main.go
@@ -1,22 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	. "github.com/sfodje/project_euler/algorithms"
 )
 
 func main() {
-	fmt.Printf("Problem 1: Sum Multiples of 3 and 5 below 1000 = %d\n", SumOfMultiplesOf3And5(1000))
-	fmt.Printf("Problem 2: Sum of Even Fibonacci Numbers <= 4,000,000 = %d\n", SumOfEvenFibonacci(4000000))
-	fmt.Printf("Problem 3: Largest Prime Factor of 600851475143 = %d\n", LargestPrimeFactor(600851475143))
-	fmt.Printf("Problem 4: Largest Palindrome From the Product of Two 3-Digit Numbers = %d\n", LargestPalidromeProduct(3))
-	multiple, _ := SmallestMultiple(1, 20)
-	fmt.Printf("Problem 5: Smallest Positive Number Evenly Divisible By All Numbers From 1 to 20 = %d\n", multiple)
-	fmt.Printf("Problem 6: Difference Between Square of Sum And Sum Of Squares From 1 to 100 = %d\n", SumSquareDifference(100))
-	fmt.Printf("Problem 7: The 10001th Prime Number = %d\n", NthPrimeNumber(10001))
-	fmt.Printf("Problem 8: The Largest Product Of 13 Adjacent Digits in the Thousand Digit Number = %d\n", GreatestProductOfNDigits(13))
-	fmt.Printf("Problem 9: The Product of the Special Pythagorean Triplet = %d\n", int64(ProductOfSpecialPythTriplet(1000)))
-	fmt.Printf("Problem 10: The Sum of All Prime Numbers Under 2,000,000 = %d\n", SumOfPrimes(2000000))
-	fmt.Printf("Problem 11: The Largest Product Of Four Adjacent Numbers In Grid = %d\n", GetMaxProduct(4))
+	problem := flag.Int("problem", 0, "Problem number")
+	end := flag.Int("end", 0, "Last problem in range of problems")
+	flag.Parse()
+
+	if *problem < 0 || *end < 0 || (*end > 0 && *problem > *end) {
+		panic("Error! Please check parameters")
+	}
+
+	if *end == 0 && *problem > 0 {
+		end = problem
+	}
+
+	for i := *problem; i <= *end; i++ {
+		if i == 0 || i == 1 {
+			fmt.Printf("Problem 1: Sum of multiples of 3 and 5 below 1000 = %d\n", SumOfMultiplesOf3And5(1000))
+		}
+
+		if i == 0 || i == 2 {
+			fmt.Printf("Problem 2: Sum of even fibonacci numbers 4,000,000 and below = %d\n", SumOfEvenFibonacci(4000000))
+		}
+
+		if i == 0 || i == 3 {
+			fmt.Printf("Problem 3: Largest prime factor of 600851475143 = %d\n", LargestPrimeFactor(600851475143))
+		}
+		if i == 0 || i == 4 {
+			fmt.Printf("Problem 4: Largest palindrome from product of 2 3-digit numbers = %d\n", LargestPalidromeProduct(3))
+		}
+		if i == 0 || i == 5 {
+			multiple, _ := SmallestMultiple(1, 20)
+			fmt.Printf("Problem 5: Smallest multiple of all numbers (1 to 20) = %d\n", multiple)
+		}
+		if i == 0 || i == 6 {
+			fmt.Printf("Problem 6: Difference between the square of sum and sum of squares (1 to 100) = %d\n", SumSquareDifference(100))
+		}
+		if i == 0 || i == 7 {
+			fmt.Printf("Problem 7: 10001th prime number = %d\n", NthPrimeNumber(10001))
+		}
+		if i == 0 || i == 8 {
+			fmt.Printf("Problem 8: Largest product of 13 adjacent digits in thousand digit number = %d\n", GreatestProductOfNDigits(13))
+		}
+		if i == 0 || i == 9 {
+			fmt.Printf("Problem 9: Product of special pythagorean triplet = %d\n", int64(ProductOfSpecialPythTriplet(1000)))
+		}
+		if i == 0 || i == 10 {
+			fmt.Printf("Problem 10: Sum of all primes under 2,000,000 = %d\n", SumOfPrimes(2000000))
+		}
+		if i == 0 || i == 11 {
+			fmt.Printf("Problem 11: Largest product of 2 adjacent numbers in number grid = %d\n", GetMaxProduct(4))
+		}
+	}
 }
