f034dcab4f579cb1c070f79ddfa5d249c0706054
Scott Fodje <sfodje@turnitin.com>
2016-03-21 11:43:32 -0700

    Simplified problem 4 solution

--- algorithms/problem4.go
+++ algorithms/problem4.go
@@ -1,11 +1,14 @@
 package algorithms
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
 
-func LargestPalidromeProduct(n int) int64 {
-	largestNumber := int64(math.Pow(10, float64(n)) - 1)
-	leastNumber := int64(math.Pow(10, float64(n-1)))
-	palindrome := int64(0)
+func LargestPalidromeProduct(n int) int {
+	largestNumber := int(math.Pow(10, float64(n)) - 1)
+	leastNumber := int(math.Pow(10, float64(n-1)))
+	palindrome := 0
 	for i := largestNumber; i >= leastNumber; i-- {
 		for j := largestNumber; j >= leastNumber; j-- {
 			product := i * j
@@ -17,56 +20,17 @@ func LargestPalidromeProduct(n int) int64 {
 	return palindrome
 }
 
-func isPalindrome(n int64) bool {
-	return firstHalf(n) == lastHalf(n)
+func isPalindrome(n int) bool {
+	return n == reverse(n)
 }
 
-func lastHalf(n int64) (half int64) {
-	numOfDigits := numOfDigits(n)
-	endDigit := numOfDigits - (numOfDigits / 2)
-	for i := numOfDigits; i >= endDigit; i-- {
-		digit := nthDigit(n, i)
-		order := float64(i - (numOfDigits / 2))
-		if numOfDigits%2 != 0 {
-			order -= 1
-		}
-		half += int64(digit) * int64(math.Pow(10, order))
+func reverse(n int) int {
+	s := strconv.Itoa(n)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-	return
-}
-
-func firstHalf(n int64) int64 {
-	numOfDigits := numOfDigits(n)
-	numOfDigitsInHalf := numOfDigits / 2
-	if numOfDigits%2 != 0 {
-		numOfDigitsInHalf += 1
-	}
-
-	return n / int64(math.Pow(10, float64(numOfDigitsInHalf)))
-}
-
-func nthDigit(n int64, i int) int {
-	divisor := int64(math.Pow(10, float64(numOfDigits(n)-i)))
-	remainder := n % divisor
-	if remainder < (divisor / 10) {
-		return 0
-	}
-	return int(firstDigit(remainder))
-}
-
-func firstDigit(n int64) int {
-	numOfDigits := numOfDigits(n) - 1
-	return int(n / int64(math.Pow(10, float64(numOfDigits))))
-}
-
-func numOfDigits(n int64) int {
-	switch {
-	case n < 0:
-		n *= -1
-		return int(math.Floor(math.Log10(float64(n)) + 1))
-	case n < 10:
-		return 1
-	default:
-		return int(math.Floor(math.Log10(float64(n)) + 1))
-	}
+	s = string(runes)
+	n, _ = strconv.Atoi(s)
+	return n
 }
